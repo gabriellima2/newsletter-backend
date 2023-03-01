97b75b0054d983017bf851bc2f74d594675d0121
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-03-01 13:51:44 -0300

    feat: adiciona validações para o caso de uso unsubscribe

--- src/use-cases/user-use-cases/impl/unsubscribe-impl.ts
+++ src/use-cases/user-use-cases/impl/unsubscribe-impl.ts
@@ -1,16 +1,17 @@
 import { DeleteUserParams, UserEntity } from "@/domain/entities/user-entity";
+import { DomainError, IDomainError } from "@/domain/errors/domain-error";
 import { UserRepository } from "@/domain/repositories/user-repository";
 import { Unsubscribe } from "../unsubscribe";
 
 export class UnsubscribeImpl implements Unsubscribe {
   constructor(
     private readonly repository: UserRepository,
-    private readonly validation: (data: DeleteUserParams) => boolean
+    private readonly validation: (data: DeleteUserParams) => IDomainError
   ) {}
 
   async execute(data: DeleteUserParams): Promise<UserEntity> {
-    const isValid = this.validation(data);
-    if (!isValid) throw new Error("Invalid values!");
+    const validated = this.validation(data);
+    if (validated.hasError) throw new DomainError(validated);
     return await this.repository.delete(data);
   }
 }
--- src/validations/user-validations/delete-user-validation.ts
+++ src/validations/user-validations/delete-user-validation.ts
@@ -1,5 +1,19 @@
-import { DeleteUserParams } from "@/domain/entities/user-entity";
+import { z } from "zod";
 
-export function deleteUserValidation(data: DeleteUserParams): boolean {
-  return true;
+import { DeleteUserParams } from "@/domain/entities/user-entity";
+import { IDomainError } from "@/domain/errors/domain-error";
+
+const userSchema = z.object({
+  id: z
+    .number({
+      invalid_type_error: "Tipo inválido. Esperava um número",
+      required_error: "O valor de ID é obrigatório",
+    })
+    .int({ message: "Tipo inválido. Esperava um número inteiro" }),
+});
+
+export function deleteUserValidation({ id }: DeleteUserParams): IDomainError {
+  const validated = userSchema.safeParse({ id });
+  if (validated.success) return { hasError: false, message: undefined };
+  return { hasError: true, message: validated.error.issues[0].message };
 }
